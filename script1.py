d139cc2a848361246db2b334968f8f830302c434
FilipRegenczuk <f.regenczuk@outlook.com>
2019-11-25 10:40:59 +0100

    change to function

--- script1.py
+++ script1.py
@@ -1,7 +1,18 @@
 import sqlite3
 
-connection = sqlite3.connect("lite.db")     # Connection to database lite.db
-cursor = connection.cursor()
-cursor.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
-connection.commit()
-connection.close()
+def create_table():
+    connection = sqlite3.connect("lite.db")     # Connection to database lite.db
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    connection.commit()
+    connection.close()
+
+def insert(item, quantity, price):
+    connection = sqlite3.connect("lite.db")     # Connection to database lite.db
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO store VALUES (?,?,?)", (item, quantity, price))
+    connection.commit()
+    connection.close()
+
+
+insert('Water', 10, 2.99)
