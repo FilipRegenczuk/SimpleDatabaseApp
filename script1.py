2c17f91abd7384f26f1d44d132009a94563b910e
FilipRegenczuk <f.regenczuk@outlook.com>
2019-11-25 21:42:43 +0100

    adding update function

--- script1.py
+++ script1.py
@@ -14,9 +14,6 @@ def insert(item, quantity, price):
     connection.commit()
     connection.close()
 
-
-#insert('Water', 10, 2.99)
-
 def view():
     connection = sqlite3.connect("lite.db")
     cursor = connection.cursor()
@@ -25,4 +22,20 @@ def view():
     connection.close()
     return rows
 
+def delete(item):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM store WHERE item=?", (item,))
+    connection.commit()
+    connection.close()
+
+def update(i, q, p):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (q,p,i))
+    connection.commit()
+    connection.close()
+
+
+update('Juice', 15, 1.95)
 print(view())
